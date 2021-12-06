dc6a8e7be0714f5304b299d3e5889db689768abd
Sergey Seletskyy <sseletskyy@gmail.com>
2021-12-07 01:22:56 +0200

    add scripts in package.json
    - test coverage
    - prettier format
    - run prettier format over src

--- src/Models/users.ts
+++ src/Models/users.ts
@@ -1,40 +1,40 @@
 export interface ApiUser {
-    login: string;
-    id: number;
-    node_id: string;
-    avatar_url: string;
-    gravatar_id: string;
-    url: string;
-    html_url: string;
-    followers_url: string;
-    following_url: string;
-    gists_url: string;
-    starred_url: string;
-    subscriptions_url: string;
-    organizations_url: string;
-    repos_url: string;
-    events_url: string;
-    received_events_url: string;
-    type: string;
-    site_admin: boolean;
-    score: number;
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+  score: number;
 }
 
 export interface ApiUsersResponse {
-    total_count: number;
-    incomplete_results: boolean;
-    items: ApiUser[];
+  total_count: number;
+  incomplete_results: boolean;
+  items: ApiUser[];
 }
 
 export interface User {
-    login: string;
-    id: number;
-    avatarUrl: string;
-    type: string;
+  login: string;
+  id: number;
+  avatarUrl: string;
+  type: string;
 }
 
 export interface UsersResponse {
-    totalCount: number;
-    incompleteResults: boolean;
-    items: User[];
+  totalCount: number;
+  incompleteResults: boolean;
+  items: User[];
 }
