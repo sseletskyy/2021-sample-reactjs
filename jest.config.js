9698d5f079f84e40dfaac4d4cefe1100784db2d4
Sergey Seletskyy <sseletskyy@gmail.com>
2021-12-06 23:58:28 +0200

    improve test configuration

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,15 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  roots: ["<rootDir>/src"],
+  clearMocks: true,
   preset: 'ts-jest',
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: [
+    "@testing-library/jest-dom/extend-expect"
+  ],
+
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
 };
